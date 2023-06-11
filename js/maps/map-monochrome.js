afe0f6b7354b8cc22d46aa6a7e0f597854dd1882
Ofentse Le Roux <85733067+ovleroux@users.noreply.github.com>
2023-06-11 22:35:53 +0200

    update

--- js/maps/map-monochrome.js
+++ js/maps/map-monochrome.js
@@ -30,8 +30,8 @@
 
 $(function() {
   // Insert Your Google Map Parameters
-  var latitude = 40.761425,
-    longitude = -73.977643,
+  var latitude = -26.131544666666663,
+    longitude = 28.377656333333334,
     map_zoom = 14;
 
   // Google Map Custom Marker Icon
